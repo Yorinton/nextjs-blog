bdbd05c4490583e5bcf6559ba5937b55e4bf01d6
Yorinton <ka2ki.yori@outlook.com>
2022-01-09 17:58:47 +0900

    cssをscssに変更

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
+import styles from './layout.module.scss'
 import Image from 'next/image'
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 
 const name = 'Yorinton'
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css' // global cssは_app.jsでしか読み込めない
+import '../styles/global.scss' // global cssは_app.jsでしか読み込めない
 
 export default function App({ Component, pageProps}) {
     return <Component {...pageProps} />
